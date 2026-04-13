c463d0daec8c08393173493cb925114004789c2e
Aryan-2204 <akadam2204@gmail.com>
2026-04-13 18:20:42 +0530

    simple plotting

---
+++ mat.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('default')
+#2d plot : numericasl and categorical/both numerical or both categorical (eg:Time Series)
+
+#simpple plotting
+price=[10,20,30,40,50]
+years=[2010,2011,2012,2013,2014]
+plt.plot(years,price)
+
+
+
